7d56f1d917cabc16d1d38801357b149ca86307b2
Jeremy Ong <jpongfam@gmail.com>
2022-09-12 23:54:58 +1000

    com

---
+++ src/lib/selector.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<h2>{name} selector</h2>
+<select {name} id={name}>
+	<option value="Major">Major</option>
+	<option value="Minor">Minor</option>
+	<option value="Pentatonic">Pentatonic</option>
+</select>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,32 @@
+<script lang="ts">
+	import Selector from "$lib/selector.svelte";
+</script>
 
 <div class="grid-container">
-  <div>sensor 1</div>
-  <div>sensor 2</div>
+	<div class="grid-item">
+		<Selector name="scale" />
+	</div>
+	<div class="grid-item">
+    <Selector name="tempo" />
+  </div>
+	<div class="grid-item">
+    <Selector name="note range" />
+  </div>
+
 </div>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-
 <style>
-  .grid-container {
-    display: inline-grid;
-    /* grid-template-columns: 2; */
-    /* gap: 5rem; */
-  }
+	.grid-container {
+		display: inline-grid;
+		/* grid-column: 1; */
+		grid-template-columns: auto auto auto;
+		gap: 2.5rem;
+	}
+	.grid-item {
+		padding: 20px;
+		text-align: center;
+		border: 1px solid;
+	}
 </style>

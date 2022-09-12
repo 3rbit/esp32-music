9799a44c23539dec702b9ee2510ac02434b79e0b
Jeremy Ong <jpongfam@gmail.com>
2022-09-12 23:12:27 +1000

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+
+<div class="grid-container">
+  <div>sensor 1</div>
+  <div>sensor 2</div>
+</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+  .grid-container {
+    display: inline-grid;
+    /* grid-template-columns: 2; */
+    /* gap: 5rem; */
+  }
+</style>

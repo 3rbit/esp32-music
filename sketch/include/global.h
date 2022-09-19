f5296f04e5e4429c19ef09d92c0e322e48b1a8b0
Jeremy Ong <jpongfam@gmail.com>
2022-09-19 22:38:03 +1000

    move files

--- sketch/include/global.h
+++ sketch/include/global.h
@@ -2,13 +2,10 @@
 #define GLOBAL_H
 
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 
 #define WIFI_SSID "PONG"
 #define WIFI_PASSWORD "curlyhair"
 
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
+WiFiServer server(80);
 
 #endif
--- sketch/src/server.cpp
+++ sketch/src/server.cpp
@@ -1,47 +1,12 @@
 /** Web server code */
 
 #include <Arduino.h>
-#include "..\include\aWOT.h"
+#include <aWOT.h>
 #include "..\include\StaticFiles.h"
 #include "..\include\server.h"
 #include "..\include\global.h"
 
 Application app;
-bool ledState = false;
-
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
-{
-  AwsFrameInfo *info = (AwsFrameInfo *)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
-  {
-    data[len] = 0;
-    if (strcmp((char *)data, "toggle") == 0)
-    {
-      ledState = !ledState;
-      ws.textAll(String(ledState));
-    }
-  }
-}
-
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len)
-{
-  switch (type)
-  {
-  case WS_EVT_CONNECT:
-    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-    break;
-  case WS_EVT_DISCONNECT:
-    Serial.printf("WebSocket client #%u disconnected\n", client->id());
-    break;
-  case WS_EVT_DATA:
-    handleWebSocketMessage(arg, data, len);
-    break;
-  case WS_EVT_PONG:
-  case WS_EVT_ERROR:
-    break;
-  }
-}
 
 void webServerSetup()
 {
@@ -55,10 +20,6 @@ void webServerSetup()
  
   app.use(staticFiles()); // serve static files
 
-  // init websocket
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-
   server.begin();
 }
 
@@ -69,5 +30,4 @@ void webServerLoop()
   if (client.connected())
     app.process(&client);
 
-  ws.cleanupClients();
 }
